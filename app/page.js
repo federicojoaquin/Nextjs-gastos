819ffd9371f38d78a3d8a384e72a37ab881cc4f8
federicojoaquin <federicojoaquing01@gmail.com>
2023-06-01 09:36:42 -0300

    New chaché

--- app/page.js
+++ app/page.js
@@ -37,7 +37,7 @@ function page() {
 
   const fetchData = async () => {
     try {
-      const response = await fetch(`https://gastosdemo-api.onrender.com/gastos?change=${offset}`);
+      const response = await fetch(`https://gastosdemo-api.onrender.com/gastos?change=${offset}`, { cache: 'no-store' });
       const gastos = await response.json();
       setGastos(gastos);
     } catch (error) {
